2423719a937c5316f3111516d7e7491c30bc9452
Diana <kdv012025@gmail.com>
2023-05-14 19:04:57 +0400

    changed file names from Form to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import './Form.css';
+import './App.css';
 
 class Form extends Component {
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Form from './Form';
+import Form from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
